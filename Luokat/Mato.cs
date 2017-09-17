34d2d345f5cf00ae3757153a20f6426da3a3c3a4
Razer2015 <razer138@gmail.com>
2017-09-17 17:31:32 +0300

    - Merged from teacher

--- Luokat/Mato.cs
+++ Luokat/Mato.cs
@@ -9,6 +9,7 @@ namespace MatoPeli.Luokat
 {
     class Mato
     {
+        private bool _kasva;
         public List<Pala> Palat { get; set; }
         public Pala Paa { get { return (this.Palat?[/*0*/ this.Palat.Count - 1] ?? null); } }
 
@@ -16,9 +17,36 @@ namespace MatoPeli.Luokat
 
         public Suunta Suunta { get; set; }
 
-        public Mato(int alkuX, int alkuY) {
+        public Mato(int alkuX, int alkuY, Suunta alkusuunta) {
+            _kasva = false;
+            Suunta = alkusuunta;
             this.Palat = new List<Pala>();
             this.Palat.Add(new Pala(alkuX, alkuY));
+        }
+
+        private void LisaaPala() {
+            if (Suunta == Suunta.OIKEA) {
+                Palat.Add(new Pala(Paa.X + 1, Paa.Y));
+            }
+            if (Suunta == Suunta.ALAS) {
+                Palat.Add(new Pala(Paa.X, Paa.Y + 1));
+            }
+            if (Suunta == Suunta.YLOS) {
+                Palat.Add(new Pala(Paa.X, Paa.Y - 1));
+            }
+            if (Suunta == Suunta.VASEN) {
+                Palat.Add(new Pala(Paa.X - 1, Paa.Y));
+            }
+        }
+
+        public void Liiku() {
+            LisaaPala();
+            if (Palat.Count >= 4 && !_kasva) {
+                Palat.RemoveAt(0);
+            }
+            else {
+                _kasva = false;
+            }
         }
     }
 }
